bd24cb855dbb16a5e98b907ec40133132f42c521
kotopia <146151278+kotopia@users.noreply.github.com>
2026-09-02 18:02:03 +0900

    wire finance import v3

--- geoflow_ops/urls.py
+++ geoflow_ops/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views_contracts, views_projects, views_employees, views_catalog, views_myinfo, views_uploads, views_events, views_workboard, views_contract_access, views_calendar
-from . import security_views, upload_guard_views, employee_security_views, event_security_views, settings_security_views, myinfo_security_views, finance_security_views, finance_import_views_v2, finance_attachment_views, finance_documents_views
+from . import security_views, upload_guard_views, employee_security_views, event_security_views, settings_security_views, myinfo_security_views, finance_security_views, finance_import_views_v3, finance_attachment_views, finance_documents_views
 from .views_home_security import tenant_home
 
 app_name = "tenant"
@@ -25,7 +25,7 @@ urlpatterns = [
     path('partners/options/', security_views.partner_options, name='partner_options'),
 
     path("finance/", finance_security_views.finance_page, name="finance_page"),
-    path("finance/import/", finance_import_views_v2.finance_import, name="finance_import"),
+    path("finance/import/", finance_import_views_v3.finance_import, name="finance_import"),
     path("finance/documents/", finance_documents_views.finance_documents, name="finance_documents"),
     path("finance/attachments/presign/", finance_attachment_views.finance_attachment_presign, name="finance_attachment_presign"),
     path("finance/attachments/commit/", finance_attachment_views.finance_attachment_commit, name="finance_attachment_commit"),
