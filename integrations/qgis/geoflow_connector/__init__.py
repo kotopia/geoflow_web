28ef204499406e493be7dee72d9a89922a3a7314
kotopia <146151278+kotopia@users.noreply.github.com>
2026-09-05 23:21:45 +0900

    Use Delta edit-state compatibility mixin

--- integrations/qgis/geoflow_connector/__init__.py
+++ integrations/qgis/geoflow_connector/__init__.py
@@ -1,4 +1,8 @@
 def classFactory(iface):
+    from .delta_apply_v2 import DeltaApplyV2Mixin
     from .plugin import GeoFlowConnectorPlugin
 
-    return GeoFlowConnectorPlugin(iface)
+    class GeoFlowConnectorPluginV051(DeltaApplyV2Mixin, GeoFlowConnectorPlugin):
+        pass
+
+    return GeoFlowConnectorPluginV051(iface)
