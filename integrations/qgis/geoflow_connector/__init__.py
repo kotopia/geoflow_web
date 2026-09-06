69098768a8e01f42f6780573613dc0b1899e5bd0
kotopia <146151278+kotopia@users.noreply.github.com>
2026-09-06 09:45:56 +0900

    Enable persistent QGIS snapshot reuse

--- integrations/qgis/geoflow_connector/__init__.py
+++ integrations/qgis/geoflow_connector/__init__.py
@@ -2,12 +2,14 @@ def classFactory(iface):
     from .delta_apply_v3 import DeltaApplyV3Mixin
     from .plugin import GeoFlowConnectorPlugin
     from .realtime_delta_v3 import RealtimeDeltaV3Mixin
+    from .snapshot_reuse import SnapshotReuseMixin
 
-    class GeoFlowConnectorPluginV063(
+    class GeoFlowConnectorPluginV070(
         RealtimeDeltaV3Mixin,
+        SnapshotReuseMixin,
         DeltaApplyV3Mixin,
         GeoFlowConnectorPlugin,
     ):
         pass
 
-    return GeoFlowConnectorPluginV063(iface)
+    return GeoFlowConnectorPluginV070(iface)
