8d08e3062bd3c7a73416ad9f656eec634b0c03b3
kotopia <146151278+kotopia@users.noreply.github.com>
2026-09-06 21:27:51 +0900

    Test QField package import token scope

--- geoflow_ops/gis/test_qfield_auth.py
+++ geoflow_ops/gis/test_qfield_auth.py
@@ -6,7 +6,9 @@ from django.test import SimpleTestCase, override_settings
 
 from .qfield_auth import (
     bearer_token_from_request,
+    issue_qfield_package_import_token,
     issue_qfield_ticket,
+    parse_qfield_package_import_token,
     parse_qfield_ticket,
     qfield_ticket_runtime_enabled,
 )
@@ -16,15 +18,20 @@ from .qfield_auth import (
 class QFieldTicketTests(SimpleTestCase):
     project_id = "11111111-1111-4111-8111-111111111401"
 
+    def _identity(self):
+        return {
+            "project_id": self.project_id,
+            "alias": "cheonan_db",
+            "group_id": "90000000-0000-4000-8000-000000000201",
+            "user_id": "1",
+            "email": "gis-dev-admin@geoflow.invalid",
+            "roles": ["tenant_admin"],
+            "perms": ["maps.view", "maps.edit"],
+        }
+
     def _issue(self):
         return issue_qfield_ticket(
-            project_id=self.project_id,
-            alias="cheonan_db",
-            group_id="90000000-0000-4000-8000-000000000201",
-            user_id="1",
-            email="gis-dev-admin@geoflow.invalid",
-            roles=["tenant_admin"],
-            perms=["maps.view", "maps.edit"],
+            **self._identity(),
             write_authorized=True,
         )
 
@@ -54,6 +61,27 @@ class QFieldTicketTests(SimpleTestCase):
             tampered = parse_qfield_ticket(token + "x", project_id=self.project_id)
         self.assertIsNone(other)
         self.assertIsNone(tampered)
+
+    def test_package_import_token_is_purpose_and_project_scoped(self):
+        with patch.dict("os.environ", {"GEOFLOW_DEV_RUNTIME_STRICT": "1"}):
+            token = issue_qfield_package_import_token(**self._identity())
+            payload = parse_qfield_package_import_token(
+                token,
+                project_id=self.project_id,
+            )
+            wrong_project = parse_qfield_package_import_token(
+                token,
+                project_id="11111111-1111-4111-8111-111111111402",
+            )
+            normal_ticket = self._issue()
+            wrong_purpose = parse_qfield_package_import_token(
+                normal_ticket,
+                project_id=self.project_id,
+            )
+        self.assertEqual(payload["purpose"], "qfield_package_import")
+        self.assertEqual(payload["alias"], "cheonan_db")
+        self.assertIsNone(wrong_project)
+        self.assertIsNone(wrong_purpose)
 
     def test_bearer_header_is_extracted(self):
         class Request:
